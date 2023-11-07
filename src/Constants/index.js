8da7b477e6297d0fca53ce4241b7e7c5c4d4617d
Ted-Kelvin-Mutwiri <tedkelvin19@gmail.com>
2023-11-07 11:42:47 +0300

    added constants folder and images folder

---
+++ src/Constants/index.js
@@ -0,0 +1,190 @@
+import { people01, people02, people03, online, budgeting, api, onboarding, facebook, instagram, linkedin, twitter, airbnb, binance, coinbase, dropbox, send, shield, star  } from "../images";
+
+export const navLinks = [
+  {
+    id: "home",
+    title: "Home",
+  },
+  {
+    id: "features",
+    title: "Features",
+  },
+  {
+    id: "product",
+    title: "Product",
+  },
+  {
+    id: "clients",
+    title: "Clients",
+  },
+];
+
+export const features = [
+  {
+    id: "feature-1",
+    icon: star,
+    title: "Rewards",
+    content:
+      "The best credit cards offer some tantalizing combinations of promotions and prizes",
+  },
+  {
+    id: "feature-2",
+    icon: shield,
+    title: "100% Secured",
+    content:
+      "We take proactive steps make sure your information and transactions are secure.",
+  },
+  {
+    id: "feature-3",
+    icon: send,
+    title: "Balance Transfer",
+    content:
+      "A balance transfer credit card can save you a lot of money in interest charges.",
+  },
+  {
+    id: "feature-4",
+    icon: online,
+    title: "Online Banking",
+    content: "Our modern web and mobile applications allow you to keep track of your finances wherever you are in the world.",
+  },
+  {
+    id: "feature-5",
+    icon: budgeting,
+    title: "Simple Budgeting",
+    content: "See exactly where your money goes each month. Receive notifications when you’re close to hitting your limits.",
+  },
+  {
+    id: "feature-6",
+    icon: onboarding,
+    title: "Fast Onboarding",
+    content: "We don’t do branches. Open your account in minutes online and start taking control of your finances right away.",
+  },
+  {
+    id: "feature-7",
+    icon: api,
+    title: "Open API",
+    content: "Manage your savings, investments, pension, and much more from one account. Tracking your money has never been easier",
+  }
+];
+
+export const feedback = [
+  {
+    id: "feedback-1",
+    content:
+      "Money is only a tool. It will take you wherever you wish, but it will not replace you as the driver.",
+    name: "Herman Jensen",
+    title: "Founder & Leader",
+    img: people01,
+  },
+  {
+    id: "feedback-2",
+    content:
+      "Money makes your life easier. If you're lucky to have it, you're lucky.",
+    name: "Steve Mark",
+    title: "Founder & Leader",
+    img: people02,
+  },
+  {
+    id: "feedback-3",
+    content:
+      "It is usually people in the money business, finance, and international trade that are really rich.",
+    name: "Kenn Gallagher",
+    title: "Founder & Leader",
+    img: people03,
+  },
+];
+
+export const stats = [
+  {
+    id: "stats-1",
+    title: "User Active",
+    value: "3800+",
+  },
+  {
+    id: "stats-2",
+    title: "Trusted by Company",
+    value: "230+",
+  },
+  {
+    id: "stats-3",
+    title: "Transaction",
+    value: "$230M+",
+  },
+];
+
+export const footerLinks = [
+  {
+    title: "Useful Links",
+    links: [
+      {
+        name: "Content",
+      },
+      {
+        name: "How it Works",
+      },
+      {
+        name: "Create",
+      },
+      {
+        name: "Explore",
+      },
+      {
+        name: "Terms & Services",
+      },
+    ],
+  },
+  {
+    title: "Community",
+    links: [
+      {
+        name: "Help Center",
+      },
+      {
+        name: "Partners",
+      },
+      {
+        name: "Suggestions",
+      },
+      {
+        name: "Blog",
+      },
+      {
+        name: "Newsletters",
+      },
+    ],
+  },
+  {
+    title: "Partner",
+    links: [
+      {
+        name: "Our Partner",
+      },
+      {
+        name: "Become a Partner",
+      },
+    ],
+  },
+];
+
+export const socialMedia = [
+  {
+    id: "social-media-1",
+    icon: instagram,
+    link: "https://www.instagram.com/",
+  },
+  {
+    id: "social-media-2",
+    icon: facebook,
+    link: "https://www.facebook.com/",
+  },
+  {
+    id: "social-media-3",
+    icon: twitter,
+    link: "https://www.twitter.com/",
+  },
+  {
+    id: "social-media-4",
+    icon: linkedin,
+    link: "https://www.linkedin.com/",
+  },
+];
