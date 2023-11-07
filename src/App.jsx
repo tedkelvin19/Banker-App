7f41d01e36c767249d2b2383c32888fba69b62d6
Ted-Kelvin-Mutwiri <tedkelvin19@gmail.com>
2023-11-07 12:28:25 +0300

    added the components folder

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,28 @@
 import React from 'react'
+import styles from './style'
 
 const App = () => {
   return (
-    <div>App</div>
+    <div className='bg-primary w-full overflow-hidden'>
+      <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+        <div className={`${styles.boxWidth}`}>
+          Navbar
+        </div>
+      </div>
+
+      <div className={`bg-primary ${styles.flexStart}`}>
+        <div className={`${styles.boxWidth}`}>
+
+        </div>
+      </div>
+
+      <div className={`bg-primary ${styles.flexStart} ${styles.paddingX}`}>
+        <div className={`${styles.boxWidth}`}>
+
+        </div>
+      </div>
+
+    </div>
   )
 }
 
