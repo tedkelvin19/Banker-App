b19fab791ffdcc84f880b86f42c4c3304c342e03
Ted-Kelvin-Mutwiri <tedkelvin19@gmail.com>
2023-11-08 11:02:06 +0300

    Added billing content

--- src/Constants/index.js
+++ src/Constants/index.js
@@ -17,20 +17,6 @@ export const navLinks = [
 
 export const features = [
   {
-    id: "feature-1",
-    icon: star,
-    title: "Rewards",
-    content:
-      "The best credit cards offer some tantalizing combinations of promotions and prizes",
-  },
-  {
-    id: "feature-2",
-    icon: shield,
-    title: "100% Secured",
-    content:
-      "We take proactive steps make sure your information and transactions are secure.",
-  },
-  {
     id: "feature-3",
     icon: send,
     title: "Balance Transfer",
@@ -68,7 +54,7 @@ export const feedback = [
     id: "feedback-1",
     content:
       "Money is only a tool. It will take you wherever you wish, but it will not replace you as the driver.",
-    name: "Herman Jensen",
+    name: "Kelvin Ted",
     title: "Founder & Leader",
     img: people01,
   },
@@ -76,7 +62,7 @@ export const feedback = [
     id: "feedback-2",
     content:
       "Money makes your life easier. If you're lucky to have it, you're lucky.",
-    name: "Steve Mark",
+    name: "Mark Mwigai",
     title: "Founder & Leader",
     img: people02,
   },
@@ -84,7 +70,7 @@ export const feedback = [
     id: "feedback-3",
     content:
       "It is usually people in the money business, finance, and international trade that are really rich.",
-    name: "Kenn Gallagher",
+    name: "Maina Kagume",
     title: "Founder & Leader",
     img: people03,
   },
