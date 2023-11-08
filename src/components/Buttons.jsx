e0651a394d27630c0ae0bad2be997ee685dd1993
Ted-Kelvin-Mutwiri <tedkelvin19@gmail.com>
2023-11-08 10:46:09 +0300

    Added buttons content

--- src/components/Buttons.jsx
+++ src/components/Buttons.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import styles from '../style'
+const Buttons = () => (
+        <button type="button" className={`py-4 px-6 font-poppins font-medium text-[18px] text-primary bg-blue-gradient rounded-[10px] outline-none ${styles}`}>
+        Get Started
+      </button>
+)
 
-const Buttons = () => {
-  return (
-    <div>Buttons</div>
-  )
-}
 
 export default Buttons
