4f847911211261224bb1384c9409d7d5bcdf6876
Ted-Kelvin-Mutwiri <tedkelvin19@gmail.com>
2023-11-08 12:08:59 +0300

    Edited the testimonials

--- src/Constants/index.js
+++ src/Constants/index.js
@@ -54,16 +54,16 @@ export const feedback = [
     id: "feedback-1",
     content:
       "Money is only a tool. It will take you wherever you wish, but it will not replace you as the driver.",
-    name: "Kelvin Ted",
-    title: "Founder & Leader",
+    name: "Candy Njeje",
+    title: "Project Designer",
     img: people01,
   },
   {
     id: "feedback-2",
     content:
       "Money makes your life easier. If you're lucky to have it, you're lucky.",
-    name: "Mark Mwigai",
-    title: "Founder & Leader",
+    name: "Kelvin Ted",
+    title: "Project Developer",
     img: people02,
   },
   {
@@ -71,7 +71,7 @@ export const feedback = [
     content:
       "It is usually people in the money business, finance, and international trade that are really rich.",
     name: "Maina Kagume",
-    title: "Founder & Leader",
+    title: "Marketing Project Manager",
     img: people03,
   },
 ];
