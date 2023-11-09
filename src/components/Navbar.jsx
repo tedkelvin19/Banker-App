fb83bdaf4f52bff089f9091b1b3845318c62956a
Ted-Kelvin-Mutwiri <tedkelvin19@gmail.com>
2023-11-09 22:34:33 +0300

    edited the navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,59 +1,23 @@
-import { useState } from "react";
-
-import { close, logo, menu } from "../images";
-import { navLinks } from "../Constants";
+import React from "react";
+import {logo} from "../images";
+import Link from "./Link";
 
 const Navbar = () => {
-  const [active, setActive] = useState("Home");
-  const [toggle, setToggle] = useState(false);
-
-  return (
+ return (
+  <header>
     <nav className="w-full flex py-6 justify-between items-center navbar">
       <img src={logo} alt="easybank" className="w-[124px] h-[32px]" />
 
-      <ul className="list-none sm:flex hidden justify-end items-center flex-1">
-        {navLinks.map((nav, index) => (
-          <li
-            key={nav.id}
-            className={`font-poppins font-normal cursor-pointer text-[16px] ${
-              active === nav.title ? "text-white" : "text-dimWhite"
-            } ${index === navLinks.length - 1 ? "mr-0" : "mr-10"}`}
-            onClick={() => setActive(nav.title)}
-          >
-            <a href={`#${nav.id}`}>{nav.title}</a>
-          </li>
-        ))}
+      <ul className="list-none sm:flex hidden justify-end items-center flex-1 text-white">
+          <li className={`font-poppins font-normal cursor-pointer text-[16px] mr-10`}><Link href="/home">Home</Link></li>
+          <li className={`font-poppins font-normal cursor-pointer text-[16px] mr-10`}><Link href="/features">Features</Link></li>
+          <li className={`font-poppins font-normal cursor-pointer text-[16px] mr-10`}><Link href="/transactions">Transactions</Link></li>
+          <li className={`font-poppins font-normal cursor-pointer text-[16px] mr-10`}><Link href="/blog">Blog</Link></li>
+          <li className={`font-poppins font-normal cursor-pointer text-[16px]`}><Link href="/accounts">Accounts</Link></li>
       </ul>
-
-      <div className="sm:hidden flex flex-1 justify-end items-center">
-        <img
-          src={toggle ? close : menu}
-          alt="menu"
-          className="w-[28px] h-[28px] object-contain"
-          onClick={() => setToggle(!toggle)}
-        />
-
-        <div
-          className={`${
-            !toggle ? "hidden" : "flex"
-          } p-6 bg-black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] rounded-xl sidebar`}
-        >
-          <ul className="list-none flex justify-end items-start flex-1 flex-col">
-            {navLinks.map((nav, index) => (
-              <li
-                key={nav.id}
-                className={`font-poppins font-medium cursor-pointer text-[16px] ${
-                  active === nav.title ? "text-white" : "text-dimWhite"
-                } ${index === navLinks.length - 1 ? "mb-0" : "mb-4"}`}
-                onClick={() => setActive(nav.title)}
-              >
-                <a href={`#${nav.id}`}>{nav.title}</a>
-              </li>
-            ))}
-          </ul>
-        </div>
-      </div>
     </nav>
+  </header>
+    
   );
 };
 
