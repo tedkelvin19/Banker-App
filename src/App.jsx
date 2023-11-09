698f77ac8fa259e774fa46751d2afccc70afcf81
Ted-Kelvin-Mutwiri <tedkelvin19@gmail.com>
2023-11-09 22:37:24 +0300

    added react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,20 @@
+import React,{useState,useEffect} from 'react'
 import styles from './style'
+import Account from './components/Account'
+import Route from './components/Route'
 
 import {Navbar, Billing,  CardDeal,  Business,  CTA,  Stats,  Footer,  Testimonials,  Main,} from './components'
-
-const App = () => (
+const App = () => {
+    const [accounts,setAccounts] = useState([])
+    useEffect(()=>{
+        fetch("http://localhost:3000/Accounts")
+        .then((res)=> res.json())
+        .then((data)=>{
+            console.log(data)
+            setAccounts(data)
+        })
+    },[])
+  return(
     <div className='bg-primary w-full overflow-hidden'>
       <div className={`${styles.paddingX} ${styles.flexCenter}`}>
         <div className={`${styles.boxWidth}`}>
@@ -12,23 +24,31 @@ const App = () => (
 
       <div className={`bg-primary ${styles.flexStart}`}>
         <div className={`${styles.boxWidth}`}>
-            <Main />
+            
+        <Route path="/home">
+          <Main />
+        </Route>
         </div>
       </div>
 
       <div className={`bg-primary ${styles.flexCenter} ${styles.paddingX}`}>
         <div className={`${styles.boxWidth}`}>
-        <Stats />
-        <Business />
-        <Billing />
-        <CardDeal />
-        <Testimonials />
-        <CTA />
-        <Footer />
+          <Route path="/features">
+          <Business />
+          </Route>
+          <Route path="/transactions">
+           <Billing />
+          </Route>
+          <Route path="/blog">
+            <Testimonials />
+          </Route>
+          <Route path="/accounts">
+          <Account accounts={accounts} />
+          </Route>
         </div>
       </div>
 
     </div>
   )
-
+  }
 export default App
