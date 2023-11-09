b3349e182c078233cfc1d4aad19c889503c2e062
Ted-Kelvin-Mutwiri <tedkelvin19@gmail.com>
2023-11-09 22:41:24 +0300

    Added link to enable navigation

---
+++ src/components/Link.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const Link = ({ className, href, children }) => {
+  // prevent full page reload
+  const onClick = (event) => {
+    event.preventDefault();
+    window.history.pushState({}, "", href)
+
+    // communicate to Routes that URL has changed
+    const navEvent = new PopStateEvent('popstate');
+    window.dispatchEvent(navEvent);
+  };
+
+  return (
+    <a className={className} href={href} onClick={onClick}>
+      {children}
+    </a>
+  );
+};
+
+export default Link;
